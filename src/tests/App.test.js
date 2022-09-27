5a363623a81be006b6898b69f92119c9d51c7d29
Kevillin Santos <kevillin_santos@hotmail.com>
2022-09-27 17:17:06 -0300

    Requisito 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
+import testData from '../../cypress/mocks/testData';
+import userEvent from '@testing-library/user-event';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+  global.fetch = jest.fn().mockResolvedValue({
+    json: () => Promise.resolve(testData),
+  });
 });
+
+describe('Testa aplicação inteira', () => {
+  test('Testa componente App', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Hello, Star Wars/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('Testa a requisição da API', async () => {
+    render(<App />);
+    const url = 'https://swapi.dev/api/planets';
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledWith(url));
+  });
+  test('Testa se a opção do Column não existe mais', () => {
+    const { getByTestId } = render(<App />);
+    const filterButton = screen.getByTestId('button-filter');
+    const columnSelect = screen.getByTestId('column-filter');
+
+    fireEvent.change(getByTestId('column-filter'), { target: { value: "orbital_period" } })
+    userEvent.click(filterButton);
+
+    // expect(columnSelect[0].value).toBe("orbital_period")
+    expect(columnSelect[0].selected).not.toBeInTheDocument();
+  })
+})
